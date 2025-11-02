c69b376259e8e9066123a978dbc23ac56933076e
prolomon <taiwooyetade67@gmail.com>
2025-11-02 21:05:11 +0100

    modify index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi atque aperiam expedita quas hic voluptates nesciunt quos accusamus, libero porro, optio natus! Hic temporibus quasi sapiente soluta. Culpa pariatur fugiat dicta praesentium quas, sunt est beatae tempora ratione earum eveniet labore expedita amet minima. Quaerat labore facilis ipsum magnam, repellendus harum voluptates, nobis ut eveniet sapiente tempore possimus delectus beatae consectetur quis eaque temporibus pariatur corporis similique iste quidem quas porro dolores. Commodi eum magni delectus! Laboriosam necessitatibus, rerum dolor minima dolorum odit, temporibus illo nemo expedita harum perferendis. Repudiandae provident iste quidem voluptas ducimus adipisci officia, blanditiis ipsam incidunt? Est ducimus magnam cum natus. Vitae magni laborum dolorum praesentium excepturi delectus nostrum consequuntur commodi nihil, autem sit, nobis tempora temporibus quia id, magnam aperiam deserunt dicta natus! Accusamus vitae quam quos unde, neque modi, excepturi ad, quae corrupti expedita vero ea reiciendis. Similique facilis assumenda aliquid possimus modi vel explicabo animi error adipisci, totam quod corrupti nam nisi quasi, aut aspernatur. Illo vero vel soluta veritatis voluptatum, enim quidem et quaerat atque inventore quos, illum debitis officiis porro sit veniam quibusdam nihil tenetur explicabo nesciunt nisi numquam esse impedit doloribus? Asperiores autem laborum nobis iusto molestias deserunt rerum delectus?</p>
+</body>
+</html>
