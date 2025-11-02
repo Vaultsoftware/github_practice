012e9157582fce744d68fb21ecf671f7d720239b
prolomon <taiwooyetade67@gmail.com>
2025-11-02 20:05:38 +0100

    modified the php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello World</title>
+</head>
+<body>
+    <h1>Hello this is my PHP code</php>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam voluptatibus eveniet ducimus debitis at possimus ab! Facilis facere ea ex iusto cum ullam doloremque quasi sit, quaerat impedit consequatur, ducimus aperiam culpa voluptate? Perspiciatis cum eius quia inventore deserunt, saepe vero labore esse veritatis dicta quam corrupti voluptates dolorem in libero est velit aut sit, itaque sed magnam earum minus. Numquam ipsum tenetur, accusamus soluta eaque nesciunt libero velit! Sint a minus autem id ducimus deserunt consectetur nobis dolor quos culpa laborum quibusdam tempore similique dolore quaerat rem quo sapiente amet, repudiandae voluptatum. Fugit corrupti numquam consectetur quisquam quod accusantium.</p>
+</body>
+</html>
